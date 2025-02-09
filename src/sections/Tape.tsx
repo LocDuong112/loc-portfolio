f4d5527b100e524d8f60da7e3561806ea0e85b3a
LocDuong112 <duongminhloc201@gmail.com>
2025-02-09 18:25:15 +0700

    About section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,66 @@
+import StarIcon from "@/assets/icons/star.svg"
+
+const words = [
+  "Performance",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+  "Reusable",
+  "Modular",
+  "Efficient",
+  "Extensible",
+  "Testable",
+  "Optimized",
+  "Readable",
+  "Well-documented",
+  "Type-safe",
+  "Intuitive",
+  "Aesthetic",
+  "Fluid",
+  "Fast-loading",
+  "Cross-browser Compatible",
+  "Mobile-first",
+  "Animation-friendly",
+  "Dark Mode Ready",
+  "Component-driven",
+  "Thread-safe",
+  "Decoupled",
+  "Error-handling Ready",
+  "Transactional",
+  "Cloud-ready",
+  "Database-efficient",
+  "Secure by Design",
+  "Adaptable",
+  "Team-oriented",
+  "Problem-solving",
+  "Growth-focused",
+  "Detail-oriented",
+  "Curious",
+  "Proactive"
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-300 overflow-x-clip -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map(word => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
